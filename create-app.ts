2284e4d97f91b8631a6fd966504a285399293d50
Ben Zörb <ben@sommerlaune.com>
2021-12-15 12:46:22 +0100

    fix: Include partials from template

--- create-app.ts
+++ create-app.ts
@@ -264,7 +264,6 @@ export async function createApp({
       '!contentful',
       '!data',
       '!content',
-      '!layout/partials',
       '!.env',
       '!package.json',
       '!package-lock.json',
