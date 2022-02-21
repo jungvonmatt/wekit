99e858e275b29650486df78dd8af2247111e8e06
Ben Zörb <benjamin.zoerb@jvm.de>
2022-02-21 22:52:54 +0100

    Build js files for storybook

--- hugo-modules/core/layouts/storybook/baseof.stories.js
+++ hugo-modules/core/layouts/storybook/baseof.stories.js
@@ -4,9 +4,18 @@
   This is just to trigger the Hugo pipes for SCSS.
   The CSS file is picked up later by Webpack.
 */}}
-{{ $css_params := .Site.Params }}
-{{ $main_css := resources.Get "scss/main.scss" | resources.ExecuteAsTemplate "style.main.scss" $css_params | toCSS (dict
+{{ $main_css := resources.Get "scss/main.scss" | toCSS (dict
   "targetPath" "css/main.css"
   "outputStyle" "compressed"
 ) }}
 {{ $main_css_permalink := $main_css.Permalink }}
+
+{{ $params := merge site.Params (dict
+  "environment" (or (getenv "HUGO_ENVIRONMENT") hugo.Environment)
+) }}
+{{ $js := resources.Get "js/main.js" | js.Build (dict
+  "format" "esm"
+  "target" "es2019"
+  "params" $params
+) }}
+{{ $js_permalink := $js.Permalink }}
